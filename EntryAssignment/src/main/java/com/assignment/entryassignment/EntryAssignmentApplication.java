7905bcdc04740402cc255ef79bf204c624e4604d
rlaisqls <rlaisqls@gmail.com>
2022-07-20 12:50:36 +0900

    project init

---
+++ EntryAssignment/src/main/java/com/assignment/entryassignment/EntryAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.entryassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryAssignmentApplication.class, args);
+    }
+
+}
---
+++ EntryAssignment/src/test/java/com/assignment/entryassignment/EntryAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.entryassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
